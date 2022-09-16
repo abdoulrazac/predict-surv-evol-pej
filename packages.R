c2516ed21fc1a5f7461a6c7b264b1090107ba5e8
abdoulrazac <38834032+abdoulrazac@users.noreply.github.com>
2022-09-16 19:33:33 +0200

    Update packages.R

--- packages.R
+++ packages.R
@@ -9,7 +9,10 @@ my_packages = c("shiny", "randomForestSRC", "shinydashboard", "shinyWidgets", "d
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
-    install.packages(p)
+    install.packages(p, dependencies = TRUE)
+  }
+  else {
+    cat(paste("Skipping already installed package:", p, "\n"))
   }
 }
 
